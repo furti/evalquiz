ad627baad751c0f676ff25ad2cc5d36ca646c926
furti <daniel.furtlehner@gmx.net>
2015-01-28 20:13:22 +0100

    used grunt to compile typescript

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    // load the task
+    grunt.loadNpmTasks("grunt-typescript");
+
+    grunt.initConfig({
+        typescript: {
+            // A specific target
+            build: {
+                src: ["ts/*.ts"],
+                dest: 'js',
+                options: {
+                    target: 'es5',
+                    module: 'commonjs',
+                    sourceMap: false,
+                    declaration: false,
+                    removeComments: false,
+                    failOnTypeErrors: true,
+                    noImplizitAny: true,
+                    basePath: 'ts',
+                    watch: 'ts'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["typescript:build"]);
+};

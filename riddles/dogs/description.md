10b9b95302034ae651f91815b51e1a8c8719505e
furti <daniel.furtlehner@gmx.net>
2015-03-09 19:48:33 +0100

    Added a riddle about two dogs and a tree

---
+++ riddles/dogs/description.md
@@ -0,0 +1,7 @@
+You are out for a walk with both of your dogs. Both dogs decide to run to their favourite tree.
+You have a big, massy and a small dog. The small one has double the speed of the big one.
+
+Both start at the same time to run to the tree. When the small dog reaches the tree he turns around and
+runs back to the big dog. Then he runs back to the tree and so forth.
+
+Could you calculate the distance your small dog traveled while running back and forth between the tree and your big dog?
---
+++ riddles/dogs/engine.js
@@ -0,0 +1,22 @@
+(function () {
+    function Engine() {
+
+    }
+
+    Engine.prototype.init = function () {
+        this.t = 5 + Math.floor(Math.random() * 5);
+        this.s = 5 + Math.floor(Math.random() * 5);
+    };
+
+    Engine.prototype.failedMessage = function () {
+        return 'The distance you calculated is not right!';
+    };
+
+    Engine.prototype.run = function (distance) {
+        var solution = distance(this.t, this.s);
+
+        return solution === this.t * 2;
+    };
+
+    return new Engine();
+})();
